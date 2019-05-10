e83486f2edb34814fbd22e0f2f7ffca45e68fd3b
Yefim Vedernikoff <yefim323@gmail.com>
2019-05-10 01:47:32 -0400

    Added all the things

---
+++ app/src/main/java/com/yefim/thedatingapp/DisplayProfileActivity.java
@@ -0,0 +1,20 @@
+package com.yefim.thedatingapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class DisplayProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_profile);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, MessageUserActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/yefim/thedatingapp/MainActivity.java
@@ -0,0 +1,67 @@
+package com.yefim.thedatingapp;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private String CHANNEL_ID = "dating-app";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        createNotificationChannel();
+    }
+
+    public void sendNotification(View view) {
+        Intent intent = new Intent(this, DisplayProfileActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.heart)
+                .setContentTitle("1 New Match")g
+                // .setContentText("You have a new Match!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                // Set the intent that will fire when the user taps the notification
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(1, builder.build());
+
+        // close the app
+        System.exit(0);
+    }
+
+    public void goToProfile(View view) {
+        Intent intent = new Intent(this, DisplayProfileActivity.class);
+        startActivity(intent);
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+}
